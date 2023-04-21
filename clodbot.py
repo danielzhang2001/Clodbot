de16a8d7b59350499a288138f3947d449da667f6
Daniel Zhang <danielz@kgsresearch.com>
2023-04-21 01:02:45 -0400

    Finished refactoring

--- clodbot.py
+++ clodbot.py
@@ -1,7 +1,13 @@
-import discord
-from discord.ext import commands
-from dotenv import load_dotenv
+"""
+The main module for running ClodBot.
+"""
+
+# pylint: disable=import-error
 import os
+import discord  # type: ignore
+from discord.ext import commands  # type: ignore
+from dotenv import load_dotenv  # type: ignore
+
 from commands.analyze import Analyze
 
 intents = discord.Intents.default()
@@ -13,11 +19,13 @@ bot = commands.Bot(command_prefix="Clodbot, ", intents=intents)
 
 @bot.event
 async def on_ready():
+    """Print a message when the bot connects to Discord."""
     print(f"{bot.user} has connected to Discord!")
 
 
 @bot.command(name='analyze')
 async def analyze_replay(ctx, *args):
+    """Analyzes replay and sends stats in a message to Discord."""
     replay_link = ' '.join(args)
     message = await Analyze.analyze_replay(replay_link)
     if message:
