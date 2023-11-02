147d4abd197ecaf7c1435aa46294f267a43adfd7
Daniel Zhang <danielz@kgsresearch.com>
2023-11-02 18:10:24 -0400

    Added support for displaying all set names if only Pokemon provided

--- smogon/set.py
+++ smogon/set.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 def get_gen_dict() -> dict:
     """Returns generation dictionary."""
     return {
@@ -18,38 +19,72 @@ def get_gen_dict() -> dict:
         "gen6": "xy",
         "gen7": "sm",
         "gen8": "ss",
-        "gen9": "sv"
+        "gen9": "sv",
     }
+
 
 def get_gen(generation: str) -> str:
     """Retrieves generation dictionary."""
     return get_gen_dict().get(generation.lower())
 
+
 def is_valid_pokemon(driver: webdriver.Chrome, pokemon: str) -> bool:
     """Check if the Pokemon name exists on the page."""
     try:
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, 
-                f"//h1[translate(text(), 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')='{pokemon.upper().replace('-', ' ')}']"))
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    f"//h1[translate(text(), 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')='{pokemon.upper().replace('-', ' ')}']",
+                )
+            )
         )
         return True
     except:
         return False
 
+
+def get_set_names(driver: webdriver.Chrome) -> list:
+    """Finds all set names on the page."""
+    try:
+        export_buttons = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "ExportButton"))
+        )
+        set_names = []
+        for export_button in export_buttons:
+            set_header = export_button.find_element(By.XPATH, "./following-sibling::h1")
+            set_names.append(set_header.text)
+        return set_names
+    except Exception as e:
+        print(f"Get All Set Names Error: {str(e)}")
+        return None
+
+
 def get_export_btn(driver: webdriver.Chrome, set: str) -> bool:
     """Finds and clicks export button for the specific set."""
     try:
         set_header = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, f"//h1[translate(text(),'abcdefghijklmnopqrstuvwxyz','ABCDEFGHIJKLMNOPQRSTUVWXYZ') = '{set.upper()}']"))
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    f"//h1[translate(text(),'abcdefghijklmnopqrstuvwxyz','ABCDEFGHIJKLMNOPQRSTUVWXYZ') = '{set.upper()}']",
+                )
+            )
         )
         export_button = WebDriverWait(set_header, 10).until(
-            EC.presence_of_element_located((By.XPATH, "./preceding-sibling::button[contains(@class, 'ExportButton')][1]"))
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    "./preceding-sibling::button[contains(@class, 'ExportButton')][1]",
+                )
+            )
         )
         export_button.click()
         return True
     except Exception as e:
         print(f"Export Button Error: {str(e)}")
         return False
+
 
 def get_textarea(driver: webdriver.Chrome, pokemon: str) -> str:
     """Finds and returns text area contents."""
