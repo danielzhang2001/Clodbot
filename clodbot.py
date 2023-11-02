c4be218348810c4d3341c7fef3c091d7bbd26018
Daniel Zhang <danielz@kgsresearch.com>
2023-11-02 18:21:28 -0400

    Fixed bug of driver being called before url is specified

--- clodbot.py
+++ clodbot.py
@@ -25,16 +25,17 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="Clodbot, ", intents=intents)
 
+
 @bot.event
 async def on_ready():
     """Print a message when the bot connects to Discord."""
     print(f"{bot.user} has connected to Discord!")
 
 
-@bot.command(name='analyze')
+@bot.command(name="analyze")
 async def analyze_replay(ctx, *args):
     """Analyzes replay and sends stats in a message to Discord."""
-    replay_link = ' '.join(args)
+    replay_link = " ".join(args)
     message = await Analyze.analyze_replay(replay_link)
     if message:
         await ctx.send(message)
@@ -42,8 +43,10 @@ async def analyze_replay(ctx, *args):
         await ctx.send("No data found in this replay.")
 
 
-@bot.command(name='giveset')
-async def give_set(ctx, pokemon: str, generation: str = None, format: str = None, *set: str):
+@bot.command(name="giveset")
+async def give_set(
+    ctx, pokemon: str, generation: str = None, format: str = None, *set: str
+):
     """Sends the Pokemon set from Smogon according to Pokemon, Generation, Format and Set. If only Pokemon provided, checks to see whether it exists or not (PLACEHOLDER)."""
     # Check if only Pokemon is provided
     if generation is None and format is None and not set:
@@ -51,16 +54,17 @@ async def give_set(ctx, pokemon: str, generation: str = None, format: str = None
         set_data = await GiveSet.fetch_set(pokemon)
     else:
         # All arguments were provided, join the set and proceed
-        set = ' '.join(set)
+        set = " ".join(set)
         set_data = await GiveSet.fetch_set(pokemon, generation, format, set)
-    
+
     error_keywords = ["not found"]
     if any(keyword in set_data for keyword in error_keywords):
         await ctx.send(set_data)
     else:
         await ctx.send(f"```{set_data}```")
 
+
 # Running Discord bot
 load_dotenv()
-bot_token = os.environ['DISCORD_BOT_TOKEN']
+bot_token = os.environ["DISCORD_BOT_TOKEN"]
 bot.run(bot_token)
