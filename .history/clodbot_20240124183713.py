5bebb03d26c9ef24cda97f0227fbbe430371d145
Daniel Zhang <danielz@kgsresearch.com>
2024-01-24 18:52:14 -0500

    deleted redundant error messages and updated driver

--- .history/clodbot_20240124183713.py
+++ .history/clodbot_20240124183713.py
@@ -56,7 +56,7 @@ async def give_set(
     elif set_data:
         await ctx.send(set_data)
     else:
-        await ctx.send(f'Pokemon "{pokemon}" not found.')
+        await ctx.send(f"Pokemon {pokemon}.")
 
 
 @bot.listen("on_message")
