791821d3330e8a0a5fee5a326e052066e0e98274
Daniel Zhang <danielz@kgsresearch.com>
2024-02-06 21:00:56 -0500

    Added inactive button to serve as header for each row

--- commands/giveset.py
+++ commands/giveset.py
@@ -14,6 +14,7 @@ class GiveSet:
 
     @staticmethod
     async def set_prompt(ctx, pokemons_data):
+        # Displays prompt with buttons for selection of Pokemon sets
         unique_id = str(uuid.uuid4())
         views = []
         prompt_text = ""
@@ -27,8 +28,15 @@ class GiveSet:
                     part.capitalize() if len(part) > 1 else part
                     for part in pokemon.split("-")
                 )
+                view.add_item(
+                    ui.Button(
+                        label=f"{formatted_name}:",
+                        style=ButtonStyle.secondary,
+                        disabled=True,
+                    )
+                )
                 for index, set_name in enumerate(sets):
-                    button_label = f"{formatted_name}: {set_name}"
+                    button_label = set_name
                     button_id = f"set_{unique_id}_{pokemon}_{index}"
                     button = ui.Button(label=button_label, custom_id=button_id)
                     view.add_item(button)
