6b7c3de2f0b78527ac3a9e862eb63c1e6d6c6c40
Daniel Zhang <danielz@kgsresearch.com>
2024-02-09 19:55:03 -0500

    testing web scraping for bulbapedia

--- .history/clodbot_20240209194412.py
+++ .history/clodbot_20240209194412.py
@@ -31,9 +31,8 @@ bot = commands.Bot(command_prefix="Clodbot, ", intents=intents)
 async def on_ready():
     # Print a message when the bot connects to Discord.
     print(f"{bot.user} has connected to Discord!")
-    # JUST FOR TESTING RANDOM POKEMON
-    pokemon_list = GiveSet.fetch_bulbasaur_name()
-    print(pokemon_list[0])
+    pokemon_list = GiveSet.fetch_random_pokemon_list()
+    print(pokemon_list)
 
 
 @bot.command(name="analyze")
