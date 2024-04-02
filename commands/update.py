e37f072dd9602778e2a7d8340877a3f7394777a5
Daniel Zhang <danielz@kgsresearch.com>
2024-04-02 13:00:57 -0400

    added raw info into google sheets functionality completed

--- commands/update.py
+++ commands/update.py
@@ -9,15 +9,17 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 def authenticate_sheets():
     creds = None
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
+    token_path = os.path.join("sheets", "token.pickle")
+    credentials_path = os.path.join("sheets", "credentials.json")
+    if os.path.exists(token_path):
+        with open(token_path, "rb") as token:
             creds = pickle.load(token)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
             creds = flow.run_local_server(port=0)
-        with open("token.pickle", "wb") as token:
+        with open(token_path, "wb") as token:
             pickle.dump(creds, token)
     return creds
