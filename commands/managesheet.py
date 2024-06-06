16868683f577995504015fa28e4ba55cde173e44
Daniel Zhang <danielz@kgsresearch.com>
2024-06-06 12:05:28 -0400

    debugging

--- commands/managesheet.py
+++ commands/managesheet.py
@@ -89,6 +89,7 @@ class ManageSheet:
                     )
                 else:
                     start_cell = f"{sheet_name}!{next_data_cell(values)}"
+                    print(f"start cell: {start_cell}")
                     add_data(
                         service,
                         spreadsheet_id,
