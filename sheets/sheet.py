c9702931e9c4e0fc2312957cd59736eda01c19c2
Daniel Zhang <danielz@kgsresearch.com>
2024-06-14 13:01:12 -0400

    added in functionality of extending columns

--- sheets/sheet.py
+++ sheets/sheet.py
@@ -911,16 +911,22 @@ def get_section_range(values: List[List[str]], player_name: str) -> str:
     return None
 
 
-def get_values(service: Resource, spreadsheet_id: str, sheet_name: str) -> List[List[str]]:
+def get_values(
+    service: Resource, spreadsheet_id: str, sheet_name: str
+) -> List[List[str]]:
     # Returns the values of the sheet.
     sheet_metadata = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
     sheets = sheet_metadata.get("sheets", [])
     for sheet in sheets:
         if sheet["properties"]["title"] == sheet_name:
-            sheet_range = sheet["properties"]["gridProperties"]
-            rows = sheet_range["rowCount"]
-            cols = sheet_range["columnCount"]
-            cell_range = f"{sheet_name}!A1:{chr(64+cols)}{rows}"
+            rows = sheet["properties"]["gridProperties"]["rowCount"]
+            cols = sheet["properties"]["gridProperties"]["columnCount"]
+            end_col = (
+                chr(64 + (cols - 1) // 26 + 65) + chr((cols - 1) % 26 + 65)
+                if cols > 26
+                else chr(64 + cols)
+            )
+            cell_range = f"{sheet_name}!A1:{end_col}{rows}"
             break
     result = (
         service.spreadsheets()
