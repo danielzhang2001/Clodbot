27f735e9ae4be9f73681d323cdd542b003dd7aea
Daniel Zhang <danielz@kgsresearch.com>
2024-09-02 15:37:03 -0400

    debugging for synchronize

--- showdown/replay.py
+++ showdown/replay.py
@@ -315,7 +315,7 @@ def process_poison(
 
             # Check the previous action to verify status condition
             if "-status" in action and ("tox" in action or "psn" in action):
-                full_action = actions[actions.index(action) - 1] + "\n" + action
+                full_action = actions[actions.index(action) - 2] + "\n" + action
                 sync_match = re.search(
                     r"\|p(\d)a: ([^\|\n]+)\|ability: Synchronize\n|-status\|p(\d)a: ([^\|\n]+)",
                     full_action,
